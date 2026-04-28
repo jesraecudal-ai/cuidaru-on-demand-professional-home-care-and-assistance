6563bd853edf80f7e3ff3b7d382f26f88518f875
Jesrae Laguna <jlcudal@gmail.com>
2026-04-28 12:59:43 +0000

    File changes

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -60,12 +60,12 @@ export default function Layout() {
                   <span className="text-xs text-gray-400">Powered by</span>
                   <img src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Stripe_Logo%2C_revised_2016.svg" alt="Stripe" className="h-4" />
                 </div>
-                <div className="flex items-center gap-2">
-                  <span className="text-xs font-semibold text-gray-600">Visa</span>
-                  <span className="text-xs font-semibold text-gray-600">Mastercard</span>
-                  <span className="text-xs font-semibold text-gray-600">Amex</span>
-                  <span className="text-xs font-semibold text-gray-600">Apple Pay</span>
-                  <span className="text-xs font-semibold text-gray-600">Google Pay</span>
+                <div className="flex items-center gap-3">
+                  <img src="https://images.seeklogo.com/logo-png/35/35/visa-logo-png_seeklogo-350035.png" alt="Visa" className="h-4 object-contain" />
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="Mastercard" className="h-5 object-contain opacity-70" />
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/American_Express_logo_%282018%29.svg" alt="Amex" className="h-5 object-contain opacity-70" />
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/b/b0/Apple_Pay_logo.svg" alt="Apple Pay" className="h-4 object-contain opacity-70" />
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Google_Pay_Logo.svg" alt="Google Pay" className="h-4 object-contain opacity-70" />
                 </div>
               </div>
             </div>
